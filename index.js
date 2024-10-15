7d203a53a4bdbcbc72cbad9f6f7b9492ea4eac30
brthapa <brthapa@maitriservices.com>
2024-10-15 10:47:25 +0545

    Script to pring signing up user uid

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-exports.helloWorld = (req, res) => {
-  res.send('Hello, World!');
-};
+const functions = require("firebase-functions");
+
+const beforeCreate = functions.auth.user().beforeCreate(async (user) => {
+  const uid = user.uid;
+  console.log("uid--->", uid);
+  return {
+    uid: uid,
+  };
+});
+
+module.exports = { beforeCreate };
